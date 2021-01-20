f3d742e894c00b2c59a6c43be1018e9c16a62005
Tarcisio2code <tarcisio.cwb@hotmail.com>
2021-01-20 16:12:24 -0300

    Adcionada a função loop para movimentar o fundo

--- HTML/Jogo-helicoptero/js/js.js
+++ HTML/Jogo-helicoptero/js/js.js
@@ -7,4 +7,22 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
+	//Principais variáveis do jogo
+	var jogo = {}
+
+	//Game Loop
+
+	jogo.timer = setInterval(loop,30); // executa a função loop a cada 30ms.
+
+	function loop(){
+		movefundo();
+	}
+
+	function movefundo(){
+		//armazena a posição atual do fundo da div fundoGame
+		esquerda = parseInt($("#fundoGame").css("background-position"));
+		//move a div fundoGame 1 pixel para a esquerda
+		$("#fundoGame").css("background-position", esquerda-1);
+	}
+
 } // Fim da função start
